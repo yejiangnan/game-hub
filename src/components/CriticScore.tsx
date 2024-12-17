230788e6ef16cd2da623384021a9a57491a2aa12
jiangnan <455301225@qq.com>
2024-12-17 23:44:45 +0800

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 50 ? "yellow" : "red";
+  return (
+    <Badge fontSize="14px" paddingX={2} borderRadius="4px" colorScheme={color}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
