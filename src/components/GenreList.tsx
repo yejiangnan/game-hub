b94b853487a416e604c584495e606c9c6de31f91
jiangnan <455301225@qq.com>
2024-12-18 23:03:48 +0800

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import useGenres from "../hooks/useGenres";
+import { Text, HStack, Image, List, ListItem } from "@chakra-ui/react";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((data) => (
-        <li key={data.id}>{data.name}</li>
+        <ListItem key={data.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={data.image_background}
+            />
+            <Text fontSize="lg">{data.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
